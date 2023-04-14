b28aa755b1e7bf72fdb93113623843963583d1ad
MarkG <markgravity.in@gmail.com>
2023-04-14 16:59:29 +0700

    [#53] Resolve Detekt warnings

--- shared/src/iosMain/kotlin/co/nimblehq/mark/kmmic/Platform.kt
+++ shared/src/iosMain/kotlin/co/nimblehq/mark/kmmic/Platform.kt
@@ -1,7 +1,9 @@
+@file:Suppress("MatchingDeclarationName")
 package co.nimblehq.mark.kmmic
 
 import platform.UIKit.UIDevice
 
+@Suppress("MatchingDeclarationName")
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
