3ddf3513830eafb314b6cb7790d5d832e60dc143
MarkG <markgravity.in@gmail.com>
2023-04-14 11:34:52 +0700

    [#53] Setup Danger report for Shared project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,51 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlinx:kover:0.6.1")
+    }
+}
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("7.3.1").apply(false)
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
     kotlin("multiplatform").version("1.7.10").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+koverMerged {
+    enable()
+
+    filters {
+        classes {
+            excludes += listOf(
+                "*.databinding.*",
+                "*.BuildConfig"
+            )
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+val detekt by configurations.creating
+
+val detektTask = tasks.register<JavaExec>("detekt") {
+    main = "io.gitlab.arturbosch.detekt.cli.Main"
+    classpath = detekt
+
+    val input = "$projectDir/shared"
+    val config = "$projectDir/shared/detekt.yml"
+    val exclude = ".*/build/.*,.*/resources/.*,**/*.kts"
+    val params = listOf("-i", input, "-c", config, "-ex", exclude)
+
+    args(params)
+}
+
+dependencies {
+    detekt("io.gitlab.arturbosch.detekt:detekt-cli:1.22.0")
 }
---
+++ shared/Dangerfile.df.kts
@@ -0,0 +1,47 @@
+@file:DependsOn("xyz.pavelkorolev.danger.detekt:plugin:1.2.0")
+
+import systems.danger.kotlin.*
+import systems.danger.kotlin.models.github.*
+import xyz.pavelkorolev.danger.detekt.DetektPlugin
+import java.io.File
+
+register.plugin(DetektPlugin)
+
+danger(args) {
+    warnDetekt()
+
+    onGitHub {
+        warnWorkInProgress()
+    }
+}
+
+fun warnDetekt() {
+    val file = File("build/reports/detekt/report.sarif")
+    if (!file.exists()) {
+        warn(
+            "🙈 No detekt report found",
+        )
+        return
+    }
+    with(DetektPlugin) {
+        val report = parse(file)
+        val count = report.count
+        if (count == 0) {
+            message("👏👏👏 Good job! Detekt found no violations here!")
+            return
+        }
+        fail(
+            "🙁 Detekt violations found: **${report.count}**.\n" +
+                "Please fix them to proceed. We have zero-warning policy"
+        )
+        report(report)
+    }
+}
+
+fun GitHub.warnWorkInProgress() {
+    if ("WIP" in pullRequest.title) {
+        warn(
+            ":construction: PR is marked with Work in Progress (WIP)",
+        )
+    }
+}
